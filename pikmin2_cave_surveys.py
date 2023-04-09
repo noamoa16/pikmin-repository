18988584574964734f4f8150dc267d5e78a99dc7
noamoa16 <noamoa16@gmail.com>
2023-04-09 16:28:06 +0900

    辺境の洞窟B7を追加

--- pikmin2_cave_surveys.py
+++ pikmin2_cave_surveys.py
@@ -139,7 +139,22 @@ def parse_data(data_str: str):
         soup.append(bold)
         soup.append(soup.new_tag('p', style = 'margin:20px'))
 
-        if stage_name == 'SR-6':
+        if stage_name == 'FC-7':
+            soup.append('オオガネモチ出現率')
+            array = np.full((4, 4), '', dtype = object)
+            array[1, 0] = '件数／確率'
+            array[2 : 4, 0] = '↓'
+            array[0, 3] = '合計'
+            array[0, 1 : 3] = ['あり', 'なし']
+            array[1 : 4, 1] = get_count_prob_tuple(result['{oogane: true}'], num_to_generate)
+            array[1 : 4, 2] = get_count_prob_tuple(result['{oogane: false}'], num_to_generate)
+            array[1 : 4, 3] = get_count_prob_tuple(num_to_generate, num_to_generate)
+            background_color = np.full((4, 4), '', dtype = object)
+            background_color[0, :] = '#d0d0d0'
+            background_color[1, :] = '#e0e0e0'
+            table = create_table(soup, array, background_color = background_color)
+            soup.append(table)
+        elif stage_name == 'SR-6':
             soup.append('オナラシ出現率')
             array = np.full((4, 4), '', dtype = object)
             array[1, 0] = '件数／確率'
