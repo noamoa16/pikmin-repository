c1adac2a4d74f7baf8de685e47140263ef3ba55c
noamoa16 <noamoa16@gmail.com>
2023-11-27 11:36:43 +0900

    相対パス指定に変更

--- freeze.py
+++ freeze.py
@@ -1,8 +1,9 @@
 from flask_frozen import Freezer
 from server import app
+from pathlib import Path
 
 freezer = Freezer(app)
-app.config['FREEZER_DESTINATION'] = 'docs'
+app.config['FREEZER_DESTINATION'] = str(Path(__file__).parent / 'docs')
 app.config['FREEZER_RELATIVE_URLS'] = True
 
 if __name__ == '__main__':
